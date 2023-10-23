6cf4cff9139440f4182db6c19abbebbfecb0723e
Jian Lik Ng <jianlik.ng@gmail.com>
2023-10-23 10:41:00 -0700

    Added controllers. Changed password and removed password hardcoding.

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://jng:CyLvWzrjJqvaUz2W@cluster0.helxukf.mongodb.net/?retryWrites=true&w=majority";
+
+var fs = require("fs");
+const mongoDB = fs.readFileSync("./secrets/mdb_conn_string.txt").toString('utf-8');
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -31,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
